4bdd1612b4939647b75ed85c9ea17ed235fac1d9
Fellipe Souza <ofellipe2023@gmail.com>
2024-08-28 18:29:54 -0300

    img apps

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,13 +1,37 @@
-﻿namespace TRINCA
+﻿using Microsoft.Maui.Controls;
+
+namespace TRINCA
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-    }
 
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            // Implementar lógica de autenticação
+            // Exemplo:
+            if (username == "admin" && password == "admin")
+            {
+                // Navegar para a próxima página ou exibir uma mensagem de sucesso
+                DisplayAlert("Login", "Login bem-sucedido!", "OK");
+            }
+            else
+            {
+                // Exibir uma mensagem de erro
+                DisplayAlert("Login", "Usuário ou senha inválidos.", "OK");
+            }
+        }
+
+        private void OnForgotPasswordTapped(object sender, EventArgs e)
+        {
+            // Navegar para a página de recuperação de senha
+            // Navigation.PushAsync(new ForgotPasswordPage());
+        }
+    }
 }
